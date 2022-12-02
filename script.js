9487b8c9ff4a643549136cef995417d7e5314b8c
mhurchaidh <bmurchie1997@gmail.com>
2022-12-02 00:22:35 -0800

    created produceSequence function

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
+const selection = [1, 2, 3, 4];
+const seq = [];
+let seqLength = 7;
+
+function produceSequence() {
+    for (let i = 0; i < seqLength; i++){
+        let randomIndex = Math.floor(Math.random() * selection.length);
+        let item = selection[randomIndex];
+        seq.push(item);
+    }
+    seqLength++;
+    console.log(seq);
+    return seq;
+}
+
+// function showSequence() {
+//     for (const element of seq){
+//         console.log(element);
+//     }
+//     setInterval(showSequence, 2000);
+// }
+
+produceSequence();
+// showSequence();
